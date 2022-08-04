500dae9215bb0c31954c937cabdf3fa895d89ba0
eduardorahal <eduardorahal@gmail.com>
2022-08-03 22:19:37 -0300

    primeira aula de backend

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Load Config
+dotenv.config({ path: './config/config.env' });
+
+// Run Server
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.listen(port, console.log("Servidor rodando na porta " + port + "..."));
+
+server.get('/', (req, res) => {
+    res.send("Página Inicial")
+});
+
+server.get('/user', (req, res) => {
+    res.send('Página do Usuário')
+})
