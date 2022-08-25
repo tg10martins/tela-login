d6fef7b7c9726f535657d2866d323e27ede86c98
eduardorahal <eduardorahal@gmail.com>
2022-08-24 22:14:31 -0300

    user registration

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import User from "../models/User.js";
 
 const user = express.Router();
 
@@ -7,7 +8,31 @@ user.get('/', (req, res) => {
 });
 
 user.post('/register', async (req, res) => {
-    console.log(req.body)
+    const { name, email, password, admin } = req.body;
+
+    const alreadyExistsUser = await User.findOne(
+        { where: { email } }
+    ).catch((err) => console.log("Error: ", err));
+
+    if (alreadyExistsUser) {
+        console.log("Usuário existente: " + alreadyExistsUser);
+        return res
+            .status(409)
+            .json({ message: "E-mail já utilizado por outro usuário"})
+    }
+
+    const newUser = new User({ name, email, password, admin });
+    const savedUser = await newUser.save().catch((err) => {
+        console.log("Error: ", err);
+        res.status(500).json({ error: "Não foi possível cadastrar o usuário"});
+    });
+
+    if (savedUser) {
+        console.log(savedUser);
+        res.json({ message: "Obrigado pelo cadastro!" })
+    }
+
+
 });
 
 export default user;
