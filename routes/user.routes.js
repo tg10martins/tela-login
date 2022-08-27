0bf54425e2d76b99379fece388f8927484cfde9f
eduardorahal <eduardorahal@gmail.com>
2022-08-26 22:24:38 -0300

    auth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import User from "../models/User.js";
+import verifyToken from "../config/auth.js";
 
 const user = express.Router();
 
 user.get('/', (req, res) => {
-    res.send('Rota de Usuários');
+    const token = req.headers['token'];
+    const authData = verifyToken(token, res);
 });
 
 user.post('/register', async (req, res) => {
