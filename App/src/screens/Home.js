49a4944dd96b80d7d7496f41d9975873c960df00
eduardorahal <eduardorahal@gmail.com>
2022-10-10 19:26:19 -0300

    dataContext

--- App/src/screens/Home.js
+++ App/src/screens/Home.js
@@ -1,17 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useContext } from 'react'
 import Teste from '../components/Teste'
-import { Provider } from '../context/dataContext'
+import { Context, Provider } from '../context/dataContext'
 
 const Home = () => {
+  const actualState = useContext(Context)
   return (
-    <View>
-      <Text>Home</Text>
-      <Provider>
-        <Teste />
-      </Provider>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+      <Text style={styles.text}>
+        {actualState.state.counter}
+      </Text>
+      <Button 
+        title="Aumentar" 
+        onPress={() => actualState.dispatch({type:"aumentar", payload:10})} 
+      />
+      { actualState.state.showMessage ? <Text>Mensagem Secreta</Text> : null }
+      <Button 
+        title="Mostrar" 
+        onPress={() => actualState.dispatch({type:"mostrar", payload: true})}
+      />
+      <Button 
+        style={styles.baton}
+        title="Esconder" 
+        onPress={() => actualState.dispatch({type:"esconder", payload: false})}
+      />
+  
     </View>
   )
 }
 
-export default Home
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30
+  }
+})
+
+export default () => {
+  return (
+    <Provider>
+      <Home />
+    </Provider>
+  )
+}
